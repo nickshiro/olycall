c2ff0110efb333b2f486ef227a309d095f06f33e
Nick <rudbsm@gmail.com>
2025-04-04 00:52:23 +0300

    Refactor aliases to @/

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
-import "@app/styles";
-import "@shared/fonts";
+import "@/app/styles";
+import "@/shared/fonts";
 
 const App: React.FC = () => {
 	return <div className="bg-red">gg</div>;
